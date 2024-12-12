2dff1dd04ae77c3754f7fcec9f831f711979d901
Harikrishna Ramesh <harikrishnaramesh777@gmail.com>
2024-12-12 12:15:02 +0530

    changes in structure

--- circumference.py
+++ circumference.py
@@ -1,9 +1,12 @@
-class circumference:
+class circumference():
     def __init__(self,radius):
         self.radius=radius
-        circumference=3.14*radius*radius
+
+    def circle(self):
+        circumference=2 * 3.14 * self.radius
         print(f"circumeference if a circle is {circumference}")
 
 rads=int(input("Enter the radius: "))
 answer=circumference(rads)
+answer.circle()
         
