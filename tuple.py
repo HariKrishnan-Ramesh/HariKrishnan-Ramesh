c2003192d0d2d1eedaae4ba938ff1709a883676b
Harikrishna Ramesh <harikrishnaramesh777@gmail.com>
2024-12-15 17:02:24 +0530

    modified : tuple.py

--- tuple.py
+++ tuple.py
@@ -1,3 +1,11 @@
 #Creating an empty tuple
 tuple1 = ()
 print(tuple1)
+
+#creating tuples with integer elements
+tuple2 = (1,2,3)
+print(tuple2)
+
+#creating with mixed datatypes
+tuple3 = (101, "Anirban", 2002, "HR Dept")
+print(tuple3)
