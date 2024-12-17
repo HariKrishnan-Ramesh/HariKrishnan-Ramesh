5d44d58093f9079cd43a2195a1deff8bc9eb59bb
Harikrishna Ramesh <harikrishnaramesh777@gmail.com>
2024-12-17 17:57:37 +0530

    commited

--- slice.py
+++ slice.py
@@ -1,6 +1,13 @@
-# string = "Harikrishna"
-# a = list(string)
-# b = a[::-1]
-# print(b)
+string = "Harikrishna"
+a = list(string)
+b = a[::-1]
+print(b)
+#output {['a', 'n', 'h', 's', 'i', 'r', 'k', 'i', 'r', 'a', 'H']}
+
+string = "Harikrishna"
+a = list(string)
+b = a[-6:-2:3]
+print(b)
+
 
 print(True or False)
